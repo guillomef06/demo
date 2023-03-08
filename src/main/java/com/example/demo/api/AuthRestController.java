beaaed1bda054d7a6bd4247c03bcc8212e3309f3
guillomef06 <guillomef06@gmail.com>
2023-03-08 23:31:02 +0100

    -Added @ResponseStatus Created when creating resources -Tests KO

--- src/main/java/com/example/demo/api/AuthRestController.java
+++ src/main/java/com/example/demo/api/AuthRestController.java
@@ -5,6 +5,7 @@ import com.example.demo.dataTransferObjects.LoginRO;
 import com.example.demo.dataTransferObjects.RegisterDTO;
 import com.example.demo.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +21,7 @@ public class AuthRestController {
     }
 
     @PostMapping("register")
+    @ResponseStatus(HttpStatus.CREATED)
     public TokenRO register(@RequestBody RegisterDTO register) {
         return this.authService.registerNewClient(register);
     }
--- src/main/java/com/example/demo/api/PixRestController.java
+++ src/main/java/com/example/demo/api/PixRestController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,6 +39,7 @@ public class PixRestController {
     }
 
     @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
     public PixDTO createPix(@RequestBody PixDTO pixDTO) {
         return this.pixService.createPix(pixDTO);
     }
